d1127387dc62f117e78d23345f0920fe14911ee7
dosunggil <chothegenius@gmail.com>
2022-05-31 09:04:54 +0900

    ff

--- SpMVC_003_SchoolV7/README.md
+++ SpMVC_003_SchoolV7/README.md
@@ -13,3 +13,23 @@
 * 만약 button 을 클릭했을 때 js코드를 사용하여 상황을 제어하고 싶으면 button 의 type 을 ```button```으로 설정한다.
 * button type 을 ```button``` 으로 설정하면 모양만 button 이고 아무런 역할을 수행하지 못한다.
 * button type 에는 ```reset``` 도 있는데 이 type 은 form tag 에 포함된 input tag 의 데이터를 모두 삭제한다.
+
+# 2022-05-30 Password 암호화하기.
+* spring-security-core 가져오기
+```
+<!-- https://mvnrepository.com/artifact/org.springframework.security/spring-security-core -->
+<dependency>
+			<groupId>org.springframework.security</groupId>
+			<artifactId>spring-security-core</artifactId>
+			<version>5.3.13.RELEASE</version>
+</dependency>
+```
+* spring/appServlet/security-context.xml 파일 생성하기.
+```
+<bean id ="passwordEncoder" class="org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder">
+		<constructor-arg index="0" value="4"/>
+</bean>
+```
+* PasswordEncoder 를 상속받은 BCryptPasswordEncoder 클래스를 사용하여 bean 선언
+* 이때 생성자에 설정한 value 값에 따라 내부에서 암호화 반복이 수행된다.
+* 이 값은 4 이상의 정수로 설정한다. (: default = 4)
--- SpMVC_003_SchoolV7/src/main/java/com/cho/school/controller/UserControllerV2.java
+++ SpMVC_003_SchoolV7/src/main/java/com/cho/school/controller/UserControllerV2.java
@@ -2,6 +2,7 @@ package com.cho.school.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +16,18 @@ import com.cho.school.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-// @RequestMapping(value = "/user")
-public class UserController {
+@Controller
+@RequestMapping(value = "/user")
+public class UserControllerV2 {
 
 	private final UserService userService;
 
-	public UserController(UserService userS) {
+	/*
+	 * @Qualifier :
+	 * UserService 인터페이스를 상속받은 클래스가 ImplV1 과 ImplV2 두개 있다.
+	 * 그 중에서 @Service("userServiceV2") 라고 명시된 클래스를 주입해 달라.
+	 */
+	public UserControllerV2(@Qualifier("userServiceV2")  UserService userS) {
 		this.userService = userS;
 	}
 
@@ -29,58 +36,39 @@ public class UserController {
 		return null;
 	}
 
-	/*
-	 * 1. login.form 에서 username 과 password 받기
-	 * 2. userVO 에 담겨서 받게 된다.
-	 * 3. userVO 를 UserService.login() 에 전달한다.
-	 * 4. UserService.login() 는 username 과 password 를 검사한다.
-	 * 5. 정상적인(username 과 password 가 일치 (유효)) 정보이면
-	 * 		 나머지 user 정보를 userVO 에 담아서 return 한다.
-	 * 		 만약 정상적인 사용자가 아니면 null 을 return 한다.
-	 * 6. Controller.login.POST method 에는 HttpSession 클래스를 매개변수로 설정된다.
-	 * 7. 정상 사용자 정보이면(userVO 가 null 이 아니면)
-	 * 		setAttribute() 로 사용자 정보를 USER 변수에 setting 한다.
-	 * 8. 정상 사용자 정보가 아니면 
-	 * 		remove() 로 혹시 모를 USER 변수를 제거한다. 
-	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(UserVO userVO, HttpSession session, Model model) {
-		// 로그인 폼에서 입력한 username, password 는 userVO 에 담겨
-		// 이곳에 도착한다.
-		log.debug(userVO.toString());
-		
-		
-		// Server 와 view 사이에서 약속된 Protocol 을 사용하기 위하여
-		// 변수를 선언하고
-		String loginMessage = null;
-		// 로그인 폼에서 전송된 데이터 중 username 으로
-		// findById() 즉 SelectOne(username) 을 실행한다.
-		// 그리고 결과를 loginUserVO 에 담는다.
-		// 만약 username 정보가 user table 에 없으면 결과는 null 이고,
-		// 정보가 있으면 관련 데이터가 포홤된 vo 가 만들어진다.
-		UserVO loginUserVO = userService.findById(userVO.getUsername());
-		
-		// username 이 가입된적 없을때
-		if(loginUserVO == null) {
-			// 가입된 적이 없다는 Keyword 를 생성하고,
-			loginMessage = "USERNAME FAIL";
-		} else
-		// username 은 있는데 password 가 다를 경우
-		if(!loginUserVO.getPassword().equals(userVO.getPassword())) {
-			// 비밀번호가 잘못 됐다는 Keyword 를 생성하고
-			loginMessage = "PASSWORD FAIL";
-		}
-
-
-		if (loginMessage == null){
-			session.setAttribute("USER", loginUserVO);
+	
+		UserVO loginUser = userService.login(userVO);
+		if(loginUser == null) {
+			session.removeAttribute("USER"); // 로그아웃 명령어
 		} else {
-			session.removeAttribute("USER");
+			session.setAttribute("USER", loginUser);
 		}
-		// view 로 보낼 message Protocol 을 setting 하기
-		model.addAttribute("LOGIN_MESSAGE",loginMessage);
-			return "user/login_ok";
+		return "redirect:/";
 	}
+	/*
+	 * HttpSession
+	 * 웹 애플리케이션은 한 번 요청을 수행하고, 응답을 받으면
+	 * 연결이 종료된다.
+	 * 이러한 것을 stateless(상태가 없다) 라고 한다.
+	 * 사용자가 로그인을 성공하면 다른 페이지로 이동했을 때 그 정보가 유지되어야 한다.
+	 * 매번 페이지 이동때 다시 로그인을 수행한다면 상당히 불편할것이다.
+	 * 
+	 * 웹 애플리케이션은 로그인한 사용자 정보를 어딘가에 보관을 하고있다가
+	 * 다른 페이지로 이동했을 때 그 정보를 참조할 수 있어야 한다.
+	 * 웹 애플리케이션은 이러한 유지되어야 할 정보를 서버의 메모리에 보관을 해놓는다.
+	 * 
+	 * Spring 에서는 이 정보를 HttpSession 클래스를 사용하여 관리한다.
+	 * 로그인한 사용자의 정보(VO 데이터)를 HttpSession 에 변수로 저장해놓는다.
+	 * 
+	 * 	session.setAttribute("USER", loginUser); 명령을 사용하여
+	 * USER 라는 변수에 loginUser 정보를 저장해놓는다.
+	 * 서버를 재시작 전까지는 USER 라는 변수에는 loginUser 정보가 계쏙 담겨져있다.
+	 * 
+	 * 임의로 이 정보를 삭제하려면
+	 * session.removeAttribute() 를 사용하여 USER 변수를 삭제해주어야 한다.
+	 */
 	
 	@RequestMapping(value="/logout", method=RequestMethod.GET)
 	public String logout(HttpSession session) {
@@ -93,7 +81,7 @@ public class UserController {
 		 * web browser 의 주소창에 localhost:8080/user/login 을 입력하고
 		 * Enter 를 눌러라
 		 */
-		return "redirect:/user/login";
+		return "redirect:/";
 	}
 	
 	@RequestMapping(value="/join",method=RequestMethod.GET)
